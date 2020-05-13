ea23230f7637f4220aac7f9b2a85619f790f702a
Jose Escobar <joanesco@espol.edu.ec>
2020-05-13 17:48:07 -0500

    feat: models added

---
+++ README.md
@@ -0,0 +1,8 @@
+# Analisis-tweets-CMU-ESPOL
+Análisis de tweets en las revueltas ocurridas en Latinoamérica a finales del 2019.
+
+## Requisitos
+* Python: 3.7.3
+* Django: 2.2.4
+* Environ: pip install django-environ
+* Agregar el .env en /analisistweets
---
+++ analisis/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+
+class Tweet(models.Model):
+    url = models.CharField(max_length=100)
+    text = models.TextField(max_length=300)
+    deleted = models.BooleanField(default=False)
+
+class TweetRelation(models.Model):
+    TYPE=[("Quote","Quote"),("Reply","Reply")]
+    tweet_source=models.ForeignKey(Tweet,on_delete=models.SET_NULL,null=True, blank=True, related_name='tweer_source')
+    tweet_response=models.ForeignKey(Tweet,on_delete=models.SET_NULL,null=True, blank=True, related_name='tweer_response')
+    relation_type=models.CharField(max_length=50, choices=TYPE)
+
+class Annotator(models.Model):
+    name = models.CharField(max_length=30)
+
+class Stance(models.Model):
+    name = models.CharField(max_length=30)
+    text = models.TextField(max_length=300)
+
+class Confidence(models.Model):
+    name = models.CharField(max_length=30)
+    text = models.TextField(max_length=300)
+    
+class Expressivity(models.Model):
+    type_exp = models.CharField(max_length=30)
+    value = models.BooleanField(default=False)
+    evidence = models.BooleanField(default=False)
+    
+class Annotation(models.Model):
+    tweet_relation = models.ForeignKey(TweetRelation,on_delete=models.SET_NULL,null=True, blank=True)
+    annotator = models.ForeignKey(Annotator,on_delete=models.SET_NULL,null=True, blank=True)
+    stance=models.ForeignKey(Stance,on_delete=models.SET_NULL,null=True, blank=True)
+    confidence=models.ForeignKey(Confidence,on_delete=models.SET_NULL,null=True, blank=True)
+    expressivity=models.ForeignKey(Expressivity,on_delete=models.SET_NULL,null=True, blank=True)
