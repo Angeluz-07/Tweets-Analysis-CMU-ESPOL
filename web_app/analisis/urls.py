dd22d68e7c410570aeaa913a738f754fb026ea60
roberto <alexandro.ramr777@gmail.com>
2020-05-23 19:32:56 -0500

    feat: add views of quotes and replies
    style: order html structure of Quotes_Simple_example.html
    
    style: move extra questions closer to main questions
    
    style: move assets at head section
    
    feat: receive form data in server

--- web_app/analisis/urls.py
+++ web_app/analisis/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('quotes', views.quotes_simple_example, name='quotes'),
+    path('replies', views.replies_simple_example, name='replies'),
 ]
--- web_app/analisis/views.py
+++ web_app/analisis/views.py
@@ -6,3 +6,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def quotes_simple_example(request):
+    if request.method == 'POST':
+        print(request.POST)    
+    return render(request, 'analisis/Quotes_Simple_example.html')
+
+def replies_simple_example(request):
+    return render(request, 'analisis/Replies_Simple_example.html')
