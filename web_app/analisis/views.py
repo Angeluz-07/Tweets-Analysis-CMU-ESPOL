23e4053fc98f347ced6a9f9aaeb3cb0a58b3d4bb
roberto <alexandro.ramr777@gmail.com>
2020-06-26 09:16:17 -0500

    refactor: :recycle: clean function to create annotation

--- web_app/analisis/views.py
+++ web_app/analisis/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http.request import QueryDict
+
 from .models import Stance, Confidence, Expressivity, Annotator, Annotation, TweetRelation, Tweet
 
 
-def get_random_tweet_relation(relation_type):
+def get_random_tweet_relation(relation_type: str) -> TweetRelation:
     # TODO : 
     # - Validate tuple (AnnotatorId,TweetRelationId) to be unique in order
     #   to avoid same annotator annotate a tweet only once. Either in DB or application
@@ -18,105 +20,63 @@ def get_random_tweet_relation(relation_type):
         if tweet_relation:
             return tweet_relation
 
+def create_annotation(form_data: QueryDict) -> None:
+    s = Stance.objects.get(name=form_data['stance'])
+    c = Confidence.objects.get(name=form_data['confidence'])
+    a = Annotator.objects.get(id=form_data['annotator_id'])
+    tr = TweetRelation.objects.get(id=form_data['tweet_relation_id'])
+
+    there_is_expressivity = form_data['expressivity_type'] != ''
+    if there_is_expressivity:
+        e = Expressivity.objects.get(
+            type=form_data['expressivity_type'],
+            value=form_data['expressivity_value'],
+            evidence=form_data['evidence']
+        )
+    else:
+        e = None
+
+    # Create Annotation
+    # TODO:
+    # - Validate tuple (AnnotatorId,TweetRelationId) to be unique in order
+    #   to avoid same annotator annotate a tweet only once. Either in DB or application
+    an, created = Annotation.objects.get_or_create(
+        tweet_relation = tr,
+        annotator=a,
+        stance=s,
+        confidence=c,
+        expressivity=e
+    )
+    print(f'{an}, created? = {created}')
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def quotes_simple_example(request):
     tweet_relation = get_random_tweet_relation("Quote")
-    print(tweet_relation.tweet_response_id)
+
     if request.method == 'POST':
-        there_is_expressivity = request.POST['expressivity_type'] != ''
-        print(request.POST)
+        create_annotation(request.POST)
         
-        s = Stance.objects.get(name=request.POST['stance'])
-        c = Confidence.objects.get(name=request.POST['confidence'])
-        if there_is_expressivity:
-            e = Expressivity.objects.get(
-                type=request.POST['expressivity_type'],
-                value = request.POST['expressivity_value'],
-                evidence = request.POST['evidence']
-            )
-
-        a = Annotator.objects.get(id=request.POST['annotator_id'])
-        tr = TweetRelation.objects.get(id=request.POST['tweet_relation_id'])
-
-        print(a)
-        print(s)
-        print(c)
-
-        # Create Annotation
-        # TODO:
-        # - Validate tuple (AnnotatorId,TweetRelationId) to be unique in order
-        #   to avoid same annotator annotate a tweet only once. Either in DB or application
-        if there_is_expressivity:
-            an, created = Annotation.objects.get_or_create(
-                tweet_relation = tr,
-                annotator=a,
-                stance=s,
-                confidence=c,
-                expressivity=e
-            )
-        else: 
-            an, created = Annotation.objects.get_or_create(
-                tweet_relation = tr,
-                annotator=a,
-                stance=s,
-                confidence=c,
-            )
-        print(f'{an}, created? = {created}')
     context = {
         'tweet_relation_id' : tweet_relation.id,
         'tweet_target_id' : tweet_relation.tweet_target_id,
         'tweet_response_id' : tweet_relation.tweet_response_id,
     }
+
     return render(request, 'analisis/home.html', context = context)
 
 def replies_simple_example(request):
     tweet_relation = get_random_tweet_relation("Reply")
-    print(tweet_relation.tweet_response_id)
+
     if request.method == 'POST':
-        there_is_expressivity = request.POST['expressivity_type'] != ''
-        print(request.POST)
+        create_annotation(request.POST)
         
-        s = Stance.objects.get(name=request.POST['stance'])
-        c = Confidence.objects.get(name=request.POST['confidence'])
-        if there_is_expressivity:
-            e = Expressivity.objects.get(
-                type=request.POST['expressivity_type'],
-                value = request.POST['expressivity_value'],
-                evidence = request.POST['evidence']
-            )
-
-        a = Annotator.objects.get(id=request.POST['annotator_id'])
-        tr = TweetRelation.objects.get(id=request.POST['tweet_relation_id'])
-
-        print(a)
-        print(s)
-        print(c)
-
-        # Create Annotation
-        # TODO:
-        # - Validate tuple (AnnotatorId,TweetRelationId) to be unique in order
-        #   to avoid same annotator annotate a tweet only once. Either in DB or application
-        if there_is_expressivity:
-            an, created = Annotation.objects.get_or_create(
-                tweet_relation = tr,
-                annotator=a,
-                stance=s,
-                confidence=c,
-                expressivity=e
-            )
-        else: 
-            an, created = Annotation.objects.get_or_create(
-                tweet_relation = tr,
-                annotator=a,
-                stance=s,
-                confidence=c,
-            )
-        print(f'{an}, created? = {created}')
     context = {
         'tweet_relation_id' : tweet_relation.id,
         'tweet_target_id' : tweet_relation.tweet_target_id,
         'tweet_response_id' : tweet_relation.tweet_response_id,
     }
+
     return render(request, 'analisis/home.html', context = context)
