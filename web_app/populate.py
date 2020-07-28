44baa4ebc283fbaa3ac3ed1d92bdac1dd798310b
roberto <alexandro.ramr777@gmail.com>
2020-07-28 02:34:19 -0500

    fix: update populate with main data

--- web_app/populate.py
+++ web_app/populate.py
@@ -38,92 +38,41 @@ def add_annotators():
 
 
 def add_tweet_and_tweet_relations():
-    # TODO: Update this code with the final format of data to feed DB.
     # Load tweet_texts in memory
     data = []
-    with open('data/tweet_database.json') as f:
+    with open('data/main_pair_database.json') as f:
         for line in f:
             data.append(json.loads(line))
 
-    tweet_text_by_tweet_id = { item['tweet_id'] : item['tweet_text'] for item in data }
-    # Add tweets
-    with open('data/pair_database.csv') as csv_file:            
-        rows = list(csv.reader(csv_file, delimiter=','))
+    #for info in data[:10]: #<-- a little limit for test and debugging
+    for info in data:
+        tweet_target_id = int(info['tweet_target_id'])
+        tweet_target_text = info['tweet_target_text']
 
-        #for row in rows[1:500]: #<-- a little limit for test and debugging
-        for row in rows[1:]:
-            response_id : str = row[1]
-            target_id : str = row[2]
+        tweet_response_id = int(info['tweet_response_id'])
+        tweet_response_text = info['tweet_response_text']
 
-            if response_id in tweet_text_by_tweet_id \
-               and target_id in tweet_text_by_tweet_id:
-                t, _ = Tweet.objects.get_or_create(
-                    id = int(response_id),
-                    text = tweet_text_by_tweet_id[response_id]
-                )
-                print(t)
+        interaction_type = info['interaction_type']
 
-                t, _ = Tweet.objects.get_or_create(
-                    id = int(target_id),
-                    text = tweet_text_by_tweet_id[target_id]
-                )
-                print(t)
+        t, _ = Tweet.objects.get_or_create(
+            id = tweet_target_id,
+            text = tweet_target_text
+        )
+        print(t)
 
-                interaction_type = row[3]
-                tr, _ = TweetRelation.objects.get_or_create(
-                    tweet_target_id = int(target_id),
-                    tweet_response_id = int(response_id),
-                    relation_type = interaction_type
-                )
-                print(tr)
+        t, _ = Tweet.objects.get_or_create(
+            id = tweet_response_id,
+            text = tweet_response_text
+        )
+        print(t)
 
+        tr, _ = TweetRelation.objects.get_or_create(
+            tweet_target_id = tweet_target_id,
+            tweet_response_id = tweet_response_id,
+            relation_type = interaction_type
+        )
+        print(tr)
 
-def fix_tweet_and_tweet_relations():
-    # TODO: Update this code with the final format of data to feed DB.
-    # Load tweet_texts in memory
-    data = []
-    with open('data/tweet_database.json') as f:
-        for line in f:
-            data.append(json.loads(line))
-
-    tweet_text_by_tweet_id = { item['tweet_id'] : item['tweet_text'] for item in data }
-    # Add tweets
-    with open('data/pair_database.csv') as csv_file:            
-        rows = list(csv.reader(csv_file, delimiter=','))
-
-        #for row in rows[1:500]: #<-- a little limit for test and debugging
-        for row in rows[1:]:
-            response_id : str = row[1]
-            target_id : str = row[2]
-
-            if response_id in tweet_text_by_tweet_id \
-               and target_id in tweet_text_by_tweet_id:
-                t = Tweet.objects.get(
-                    id = int(response_id),
-                    #text = tweet_text_by_tweet_id[response_id].encode('unicode_escape')
-                )
-                print(t)
-
-                t.text = tweet_text_by_tweet_id[response_id]
-                t.save()
-
-                t = Tweet.objects.get(
-                    id = int(target_id),
-                    #text = tweet_text_by_tweet_id[target_id].encode('unicode_escape')
-                )
-                print(t)
-
-                t.text = tweet_text_by_tweet_id[target_id]
-                t.save()
-                """
-                interaction_type = row[3]
-                tr, _ = TweetRelation.objects.get_or_create(
-                    tweet_target_id = int(target_id),
-                    tweet_response_id = int(response_id),
-                    relation_type = interaction_type
-                )
-                print(tr)
-                """
 
 def add_questions():
     sections = [
@@ -233,8 +182,7 @@ def add_questions():
             print(q)
 
 if __name__ == '__main__':
-    print("Starting population script...")
-    add_questions()
+    print("Starting population script...")    
     add_tweet_and_tweet_relations()
-    #fix_tweet_and_tweet_relations()
+    add_questions()
     add_annotators()
