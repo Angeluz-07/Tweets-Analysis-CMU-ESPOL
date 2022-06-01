b84e74c39970a5047929103acf885db1ecbd1549
Roberto Mena <alexandro.ramr777@gmail.com>
2022-05-31 22:28:59 -0500

    mark current tweets with 0 annotations as no_relevant

--- web_app/script_update_no_relevant.py
+++ web_app/script_update_no_relevant.py
@@ -30,17 +30,17 @@ def update_no_relevant(debug):
         return result
 
     print("\tCount before update")
-    for i in range(0,2+1):        
-        print(f'tweets wit {i} annotations = {get_trs_count(i)}')
+    for i in range(0,2+1):
+        print(f'tweets with {i} annotations = {get_trs_count(i)}')
 
     ids = []
-    for i in range(0,2+1):
+    for i in [0]:
         queryset = get_trs(i)
         ids+= queryset.values_list('id', flat=True)
     print(f'# items to save  = {len(ids)} ; {ids[:5]}...')
 
     import json
-    outfilename = 'data/20220514_ids_marked_as_no_relevant.json'
+    outfilename = 'data/20220531_ids_marked_as_no_relevant.json'
     with open(outfilename, 'w') as outfile:
         json.dump(ids, outfile)
     print(f'saved in = {outfilename}')
@@ -57,7 +57,7 @@ def update_no_relevant(debug):
 
     print("\tCount after update")
     for i in range(0,2+1):        
-        print(f'tweets wit {i} annotations = {get_trs_count(i)}')
+        print(f'tweets with {i} annotations = {get_trs_count(i)}')
 
 if __name__ == '__main__':
     update_no_relevant(debug=True)
